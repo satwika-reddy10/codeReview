63cd2f6f01ba8ee317ca149b6545ff42672ce0da
VarshiniGopaldas <varshinigopaldas@gmail.com>
2025-08-18 12:33:10 +0530

    Added LoginNavigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import LandingPage from './components/LandingPage/LandingPage';
 import LoginPage from './components/LoginPage/LoginPage';
 const router = createBrowserRouter([
   {
     path: '/',
+    element: <LandingPage />,
+  },
+  {
+    path: '/login',
     element: <LoginPage />,
   },
 ]);
